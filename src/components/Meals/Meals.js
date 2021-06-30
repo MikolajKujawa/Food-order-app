9e3c3c56313bd0a3c63acd56c3b989b51dccacd3
Mikołaj Kujawa <mkkujawix@gmail.com>
2021-06-30 15:49:24 +0200

    Create list of meal Add Cart component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvaliableMeals';
+
+const Meals = () => {
+    return ( 
+        <>
+            <MealsSummary/>
+            <AvailableMeals/>
+        </>
+     );
+}
+ 
+export default Meals;

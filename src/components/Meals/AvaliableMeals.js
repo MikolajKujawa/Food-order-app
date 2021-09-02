aafdc846eb5da8f87acb9b0df492da933e24ed6e
Mikołaj Kujawa <mkkujawix@gmail.com>
2021-09-02 21:23:32 +0200

    Error handling with bad data download.

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -7,10 +7,16 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-test-581be-default-rtdb.europe-west1.firebasedatabase.app/meals.json').then();
+
+      if(!response.ok){
+        throw new Error('Something went wronk!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -27,7 +33,10 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+      fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });  
   }, []);
 
   if (isLoading) {
@@ -37,6 +46,14 @@ const AvailableMeals = () => {
       </section>
     );
   };
+
+  if (httpError) {
+    return(
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
 
     const mealsList = meals.map((meal) => (
       <MealItem 

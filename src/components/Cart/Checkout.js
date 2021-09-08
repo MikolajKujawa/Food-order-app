289ed7064f99b70c92aa80b348c141a264dc4611
Mikołaj Kujawa <mkkujawix@gmail.com>
2021-09-08 21:05:58 +0200

    Sending the order to the database

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -40,9 +40,15 @@ const Checkout = (props) => {
 
     const formIsValid = enteredNameIsValid && enteredStreetIsValid && enteredPostalCodeIsValid && enteredCityIsValid;
 
-    if(formIsValid){
+    if(!formIsValid){
         return;
     }
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postalCode: enteredPostalCode
+    });
   };
 
   const nameControlClasses = `${classes.control} ${formInputsValidity.name ? '' : classes.invalid}`;
